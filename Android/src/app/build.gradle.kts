e8e7146cd252ebaf0397f6f740e98ad2428e3535
ligang2025 <xiaoli49401@gmail.com>
2026-04-30 12:34:38 +0800

    Update build.gradle.kts

--- Android/src/app/build.gradle.kts
+++ Android/src/app/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.api.variant.ApplicationVariant
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -14,7 +12,6 @@ android {
         applicationId = "com.google.ai.edge.gallery"
         minSdk = 24
         targetSdk = 34
-
         versionCode = 1
         versionName = "1.0.0"
     }
@@ -32,23 +29,5 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-androidComponents {
-    onVariants { variant ->
-        variant.outputs.forEach { output ->
-            val versionName = variant.versionName.getOrElse("1.0.0")
-            val buildTime = java.time.LocalDateTime.now()
-                .toString()
-                .replace(":", "-")
-            output.outputFileName.set(
-                "Gallery-${variant.name}-v${versionName}-${buildTime}.apk"
-            )
-        }
     }
 }
